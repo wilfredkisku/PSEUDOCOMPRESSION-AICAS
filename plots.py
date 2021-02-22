c763720c0954d29c8b4c7f6bf7b90ef6c5274b07
wilfredkisku <wilfred.kisku@gmail.com>
2021-02-22 19:40:34 +0530

    changes in plots

--- plots.py
+++ plots.py
@@ -1,5 +1,8 @@
+import tarfile
+from io import BytesIO
 import numpy as np
 import matplotlib.pyplot as plt
+from urllib.request import urlopen
 
 def testOne():
     #np.random.seed(444)
@@ -44,7 +47,40 @@ def testTwo():
     ax2.yaxis.tick_right()
     plt.show()
 
+def add_titlebox(ax, title):
+    ax.text(.55, .8, title, horizontalalignment='center', transform=ax.transAxes, bbox=dict(facecolor='white', alpha=0.6), fontsize=12.5)
+    return ax
+
+def testThree():
+    fpath = 'data/cal_housing.data'
+    with open(fpath, mode='r') as archive:
+        housing = np.loadtxt(archive,delimiter=',')
+        #housing = np.loadtxt(archive.extractfile(fpath), delimiter=',')
+
+    y = housing[:,-1]
+    pop, age = housing[:,[4,7]].T
+
+    gridsize = (3, 2)
+    fig = plt.figure(figsize=(12, 8))
+    ax1 = plt.subplot2grid(gridsize, (0, 0), colspan=2, rowspan=2)
+    ax2 = plt.subplot2grid(gridsize, (2, 0))
+    ax3 = plt.subplot2grid(gridsize, (2, 1))
+
+    ax1.set_title('Home value as a function of home age & area population',fontsize=14)
+    sctr = ax1.scatter(x=age, y=pop, c=y, cmap='RdYlGn')
+    plt.colorbar(sctr, ax=ax1, format='$%d')
+    ax1.set_yscale('log')
+    ax1.set_ylabel('population')
+    ax1.set_xlabel('age')
+    ax2.hist(age, bins='auto')
+    ax3.hist(pop, bins='auto', log=True)
+
+    add_titlebox(ax2, 'Histogram: home age')
+    add_titlebox(ax3, 'Histogram: area population (log scl.)')
+    plt.show()
+
 if __name__ == "__main__":
 
     #testOne()
-    testTwo()
+    #testTwo()
+    testThree()
